3ab0c44fd8f4dbdc0fdc9cbc4ee0c7bb3bafc6fc
Nikolay Kartyshov <nikkartyshov@gmail.com>
2018-02-01 15:47:36 +0300

    Minor fix

--- adguard_cb/src/main/java/com/adguard/android/contentblocker/SettingsActivity.java
+++ adguard_cb/src/main/java/com/adguard/android/contentblocker/SettingsActivity.java
@@ -1,6 +1,6 @@
 /**
  * This file is part of Adguard Content Blocker (https://github.com/AdguardTeam/ContentBlocker).
- * Copyright © 2016 Performix LLC. All rights reserved.
+ * Copyright © 2018 Adguard Software Ltd. All rights reserved.
  * <p>
  * Adguard Content Blocker is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by the
